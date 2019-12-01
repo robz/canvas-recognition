ddcf1e5d5c65a8cec95412f62b170ae2b8b2c64f
Robby Nevels <rjnevels@gmail.com>
2019-11-30 23:55:43 -0600

    disable buttons again, clean histoy script

--- src/get_history.js
+++ src/get_history.js
@@ -4,19 +4,34 @@ const keys = require('./keys');
 
 const pubnub = new PubNub(keys);
 
+if (process.argv.length !== 3) {
+  console.log('usage: node get_history.js number_of_messages');
+  process.exit();
+}
+
+const count = Number(process.argv[2]);
+console.log(`requesting ${count} messages`);
+
 pubnub.history(
   {
     channel: 'digit_prediction_mistakes',
-    count: 10,
+    count,
     stringifiedTimeToken: true,
   },
-  function(status, response) {
+  (status, response) => {
     const data = JSON.stringify(response.messages, null, '  ');
-    console.log(data);
-    fs.writeFile(
-      `data/messages-${new Date().toString().replace(/ /g, '_')}.json`,
-      data,
-      () => {},
+
+    const uuidCounts = response.messages.reduce(
+      (a, e) => a.set(e.entry.uuid, 1 + (a.get(e.entry.uuid) || 0)),
+      new Map(),
     );
+    console.log('uuids:', uuidCounts);
+
+    console.log(`received ${response.messages.length} responses`);
+    const filename = `recorded-mistakes/messages-${new Date()
+      .toString()
+      .replace(/ /g, '_')}.json`;
+
+    fs.writeFile(filename, data, () => console.log('wrote to ' + filename));
   },
 );
--- src/index.js
+++ src/index.js
@@ -149,6 +149,7 @@ async function main() {
         },
       };
       pubnub.publish(publishConfig);
+      disableButtons();
     };
   }
 
